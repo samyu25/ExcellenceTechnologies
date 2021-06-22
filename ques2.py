bff26233205d3538e8eaf0400362a134d44adcf1
samyu25 <60413333+samyu25@users.noreply.github.com>
2021-06-22 18:25:57 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -1,13 +1,18 @@
 import requests
 import json
-link = "https://reqres.in/api/users"
-res = json.loads(requests.get(link).text)
-b = list(res.values())[4]
-count = 0
-for i in range(len(b)):
-	print(b[i]['first_name']+' '+b[i]['last_name'])
-	count+=1
-print(count)
+import asyncio
+async def main():
+	count = 0
+	for i in range(12):
+			currentUrl = f'https://reqres.in/api/users?page={i}'
+			print(currentUrl)
+			res = json.loads(requests.get(currentUrl).text)
+			b = list(res.values())[4]
+			for i in range(len(b)):
+				print(b[i]['first_name']+' '+b[i]['last_name'])
+				count+=1
+			print(count)
+asyncio.run(main())
 
 
 

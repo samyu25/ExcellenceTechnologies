ea851f588d75a1ae8d06441ca756d61dfa6f686b
samyu25 <60413333+samyu25@users.noreply.github.com>
2021-06-22 11:20:46 +0530

    Update ques2.py

--- ques2.py
+++ ques2.py
@@ -3,8 +3,11 @@ import json
 link = "https://reqres.in/api/users"
 res = json.loads(requests.get(link).text)
 b = list(res.values())[4]
+count = 0
 for i in range(len(b)):
 	print(b[i]['first_name']+' '+b[i]['last_name'])
+	count+=1
+print(count)
 
 
 
